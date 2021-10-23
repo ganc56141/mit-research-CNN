aa604e8e3a3b02368abfb9c0159323002fbe1775
Felix <20305244+ganc56141@users.noreply.github.com>
2021-10-23 14:46:20 +0800

    Update server.py
    adjusted CNN usage

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import sys
-import random
-sys.path.append("/home/ec2-user/deployedapp/cancer.py")
-
-# from cancer import predict_using_CNN
+sys.path.append("/home/ec2-user/deployedapp/")
+from cancer-detection-cnn import predict_using_CNN, convert_to_jpg
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -17,11 +15,17 @@ def index():
         data = request.get_json(force=True)
         print(len(data['data_url']), file=sys.stderr)
         print(data, file=sys.stderr)
-        if random.random() > 0.2:
-            return f"There is a {random.uniform(0.5, 0.99)} chance that you have cancer. Please seek medical attention immediately"
+        
+        imgstring = data['data_url']
+        img_path = convert_to_jpg(imgstring)
+        res = predict_using_CNN(img_path)
+        if res == 'positive':
+            return f"There is a high chance that you have cancer. Please seek medical attention immediately"
+        elif res == 'negative':
+            return f"You most likely do not have cancer. Continue staying healthy!"
         else:
-            return f"You most likely do not have cancer (<{random.uniform(0.01, 0.2)}. Continue staying healthy!"
+            return f'cannot classify. you... are a special case'
     else:
-        return 'Hello!'
+        return 'Hello! Make a post request with the patient\'s image.'
 
 app.run(host='0.0.0.0', port=80)
